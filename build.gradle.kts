16f45f762131c243484d6488beddc7da152e3bbe
Sindrow33 <108239153+Sindrow33@users.noreply.github.com>
2026-09-19 09:11:50 +0300

    creator: новый аниме-расширение для Aniyomi — build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,30 @@
-buildscript {
-    dependencies {
-        classpath(libs.kotlin.gradle)
-    }
-}
-
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
-
-    alias(kei.plugins.spotless)
+    id("com.android.library")
+    id("kotlin-android")
 }
 
-val buildLogic: IncludedBuild = gradle.includedBuild("build-logic")
-tasks {
-    listOf("clean", "spotlessApply", "spotlessCheck").forEach { task ->
-        named(task) {
-            dependsOn(buildLogic.task(":$task"))
+android {
+    compileSdk = 31
+    sourceSets {
+        getByName("main").apply {
+            manifest.srcFile("src/ru/myanimelist/AndroidManifest.xml")
         }
     }
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 31
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
