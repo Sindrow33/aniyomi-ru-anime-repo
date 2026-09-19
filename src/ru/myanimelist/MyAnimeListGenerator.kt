219e496a2079a7c3a4d00483a8a65fef6462efc8
Sindrow33 <108239153+Sindrow33@users.noreply.github.com>
2026-09-19 09:14:26 +0300

    revert creator

--- src/ru/myanimelist/MyAnimeListGenerator.kt
+++
@@ -1,9 +0,0 @@
-package keiyoushi.myanimelist
-
-import eu.kanade.tachiyomi.source.SourceFactory
-
-class MyAnimeListGenerator : SourceFactory {
-    override fun createSources(): List<Source> {
-        return listOf(MyAnimeList())
-    }
-}
